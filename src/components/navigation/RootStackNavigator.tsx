fa89e6120bdb4077b54fe79b448f6b3d79fbec58
hyochan <dooboolab@gmail.com>
2019-11-02 22:39:27 +0900

    Enhace typing

--- src/components/navigation/RootStackNavigator.tsx
+++ src/components/navigation/RootStackNavigator.tsx
@@ -1,12 +1,17 @@
 import Intro from '../screen/Intro';
 import { NavigationNativeContainer } from '@react-navigation/native';
 import React from 'react';
+import { ScreenProps } from '../../types';
 import Temp from '../screen/Temp';
 import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
-function RootNavigator({ screenProps }): React.ReactElement {
+function RootNavigator({
+  screenProps,
+}: {
+  screenProps: ScreenProps;
+}): React.ReactElement {
   const { theme } = screenProps;
   return (
     <NavigationNativeContainer>
