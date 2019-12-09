04b3fc5a485e32fc7c1a9d5ec16afdaf866f63b7
hyochan <dooboolab@gmail.com>
2019-12-09 20:55:17 +0900

    Enhance test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,16 @@
 module.exports = {
   preset: 'jest-expo',
-  setupFiles: ['./test/jestSetup.ts'],
+  automock: false,
+  setupFiles: [
+    './test/jestSetup.ts',
+    './node_modules/react-native-gesture-handler/jestSetup.js',
+  ],
   transformIgnorePatterns: [
     /* eslint-disable */
     'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|sentry-expo|native-base|@dooboo-ui/native)',
     /* eslint-enable */
   ],
-  testMatch: [
-    '**/__tests__/**/*.ts?(x)',
-    '**/?(*.)+(spec|test).ts?(x)',
-  ],
+  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
   globals: {
     'ts-jest': {
       tsConfig: {
@@ -36,8 +37,13 @@ module.exports = {
     'android.ts',
     'android.tsx',
   ],
-  // 'moduleNameMapper': {
-  //   '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|
-  // woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':'<rootDir>/test/assetsTransformer.js'
+  // moduleNameMapper: {
+  //   '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+  //     '<rootDir>/test/assetsTransformer.js',
   // },
+  haste: {
+    defaultPlatform: 'ios',
+    platforms: ['android', 'ios', 'native'],
+    providesModuleNodeModules: ['react', 'react-native'],
+  },
 };
