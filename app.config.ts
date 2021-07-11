b19d4e151877c1f69abacd224253b35a9ce77387
Hyo <dooboolab@gmail.com>
2021-07-11 12:54:48 +0900

    Expo app config with types

--- app.config.ts
+++ app.config.ts
@@ -1,29 +1,32 @@
-export default {
-  expo: {
-    name: 'dooboo',
-    slug: 'dooboo-expo',
-    privacy: 'public',
-    platforms: ['ios', 'android', 'web'],
-    version: '1.0.0',
-    orientation: 'default',
-    icon: './assets/icon.png',
-    splash: {
-      image: './assets/splash.png',
-      resizeMode: 'contain',
-      backgroundColor: '#ffffff',
-    },
-    updates: {
-      fallbackToCacheTimeout: 0,
-    },
-    assetBundlePatterns: ['**/*'],
-    userInterfaceStyle: 'automatic',
-    ios: {
-      supportsTablet: true,
-    },
-    android: {
-      userInterfaceStyle: 'dark',
-      useNextNotificationsApi: true,
-    },
-    description: 'Starter project from dooboo-cli.',
+import 'dotenv/config';
+
+import {ConfigContext, ExpoConfig} from '@expo/config';
+
+export default ({config}: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: 'dooboo',
+  slug: 'dooboo-expo',
+  privacy: 'public',
+  platforms: ['ios', 'android', 'web'],
+  version: '1.0.0',
+  orientation: 'default',
+  icon: './assets/icon.png',
+  splash: {
+    image: './assets/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
   },
-};
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ['**/*'],
+  userInterfaceStyle: 'automatic',
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    userInterfaceStyle: 'dark',
+    useNextNotificationsApi: true,
+  },
+  description: 'Starter project from dooboo-cli.',
+});
