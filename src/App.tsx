ebbd204e2dcf0eab8e5af32a077ef74e8ea21a0f
hyochan <dooboolab@gmail.com>
2022-05-14 21:32:27 +0900

    Migrate expo-app-loading to expo-splash-screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,69 @@
 import * as SplashScreen from 'expo-splash-screen';
 
-import React, {useEffect} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 
-import AppLoading from 'expo-app-loading';
 import Icons from './utils/Icons';
 import RootNavigator from './components/navigations/RootStack';
 import RootProvider from './providers';
+import {StatusBarBrightness} from 'dooboo-ui';
+import {View} from 'react-native';
 import {gestureHandlerRootHOC} from 'react-native-gesture-handler';
 import {useAssets} from 'expo-asset';
 import {useFonts} from 'expo-font';
 
 SplashScreen.preventAutoHideAsync();
 
-function App(): React.ReactElement {
+function App(): React.ReactElement | null {
   const [fontsLoaded] = useFonts({
     IcoMoon: require('dooboo-ui/Icon/doobooui.ttf'),
   });
 
   const [assets] = useAssets(Icons);
+  const [appIsReady, setAppIsReady] = useState(false);
 
   useEffect(() => {
+    const prepare = async (): Promise<void> => {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    };
+
     if (assets && fontsLoaded) {
       SplashScreen.hideAsync();
     }
+
+    prepare();
   }, [assets, fontsLoaded]);
 
-  if (!assets) {
-    return <AppLoading />;
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
   }
 
-  return <RootNavigator />;
+  return (
+    <View
+      style={{flex: 1, flexDirection: 'column'}}
+      onLayout={onLayoutRootView}
+    >
+      <StatusBarBrightness />
+      <RootNavigator />
+    </View>
+  );
 }
 
 function ProviderWrapper(): React.ReactElement {
